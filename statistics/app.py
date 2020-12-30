6eb7703bb43909e22fc2d2f56ba77f83caa86ab9
Yevgeniy Kiveisha <yevgeniy.kiveisha.com>
2020-12-30 14:01:58 +0200

    commit
    Signed-off-by: Yevgeniy Kiveisha <yevgeniy.kiveisha.com>

--- statistics/app.py
+++ statistics/app.py
@@ -8,6 +8,7 @@ import pandas as pd
 import sqlite3
 import argparse
 import datetime
+import time
 
 def Load(filename):
 	if os.path.isfile(filename) is True:
@@ -33,6 +34,14 @@ def signal_handler(signal, frame):
 	print("Accepted signal from other app")
 	g_exit = True
 
+def GetStockInfo(curs, ticker):
+	query = "SELECT * FROM stocks_info WHERE `ticker` = '{0}'".format(ticker)
+	curs.execute(query)
+	rows = curs.fetchall()
+	if len(rows) > 0:
+		return rows[0]
+	return None
+
 def GetStocks(curs, price, year, month, day):
 	if month < 10:
 		month = '0{0}'.format(month)
@@ -53,13 +62,73 @@ def GetStocks(curs, price, year, month, day):
 			tickers_list.append(row[0])
 	return buying_stocks, tickers_list
 
+def GetStocksByTS(curs, price, year, month, day):
+	if month < 10:
+		month = '0{0}'.format(month)
+	if day < 10:
+		day = '0{0}'.format(day)
+	
+	d = datetime.date(int(year),int(month),int(day))
+	unixtime = time.mktime(d.timetuple())
+
+	buying_stocks 	= []
+	tickers_list 	= []
+	query = "select stocks_price.* from (select timestamp from stocks_price where close < {close} and vol > 0 and timestamp > {ts} ORDER BY timestamp LIMIT 1) as tblTS inner join stocks_price on stocks_price.timestamp = tblTS.timestamp where stocks_price.close < {close} and stocks_price.vol > 0".format(close=price,ts=unixtime)
+	# print(query)
+	curs.execute(query)
+	rows = curs.fetchall()
+	if len(rows) > 0:
+		for row in rows:
+			buying_stocks.append({
+				"ts": row[0],
+				"date": row[1],
+				"ticker": row[2],
+				"price": row[5]
+			})
+			tickers_list.append(row[0])
+	return buying_stocks, tickers_list
+
+def GetStockByTS(curs, ticker, year, month, day):
+	if month < 10:
+		month = '0{0}'.format(month)
+	if day < 10:
+		day = '0{0}'.format(day)
+
+	d = datetime.date(int(year),int(month),int(day))
+	unixtime = time.mktime(d.timetuple())
+
+	query = "select * from stocks_price where ticker = '{ticker}' and timestamp > {ts} ORDER BY timestamp LIMIT 1".format(ticker=ticker,ts=unixtime)
+	# print(query)
+	curs.execute(query)
+	rows = curs.fetchall()
+	if len(rows) > 0:
+		return {
+			"ts": rows[0][0],
+			"date": rows[0][1],
+			"ticker": rows[0][2],
+			"price": rows[0][5]
+		}
+	return None
+
+def GetClosestStocks(curs, price, year, month, day):
+	re_day = day
+	tries  = 0
+	while(tries < 10):
+		#stocks, tickers = GetStocks(curs,price,year,month,re_day)
+		stocks, tickers = GetStocksByTS(curs,price,year,month,re_day)
+		if stocks is not None:
+			return stocks, tickers
+		re_day += 1
+		tries  += 1
+	return None, None
+
 def GetStock(curs, ticker, year, month, day):
 	if month < 10:
 		month = '0{0}'.format(month)
 	if day < 10:
 		day = '0{0}'.format(day)
 	query = "select ticker, close from stocks_price where ticker = '{ticker}' and date = '{year}-{month}-{day} 00:00:00'".format(ticker=ticker,year=year,month=month,day=day)
-	print(query)
+	# print(query)
 	curs.execute(query)
 	rows = curs.fetchall()
 	if len(rows) > 0:
@@ -69,6 +138,18 @@ def GetStock(curs, ticker, year, month, day):
 		}
 	return None
 
+def GetClosestStock(curs, ticker, year, month, day):
+	re_day = day
+	tries  = 0
+	while(tries < 10):
+		#stock = GetStock(curs,ticker,year,month,re_day)
+		stock = GetStockByTS(curs,ticker,year,month,re_day)
+		if stock is not None:
+			return stock
+		re_day += 1
+		tries  += 1
+	return None
+
 def GetDay(year, month, day):
 	query = "{year}-{month}-{day}".format(year=year,month=month,day=day)
 	date_time_obj = datetime.datetime.strptime(query, '%Y-%m-%d')
@@ -76,7 +157,6 @@ def GetDay(year, month, day):
 
 def GetWorkingDate(year, month, day):
 	weekday = GetDay(year, month, day)
-	#print(weekday)
 	if weekday == 5:
 		day += 2
 	elif weekday == 6:
@@ -84,10 +164,29 @@ def GetWorkingDate(year, month, day):
 	
 	return year, month, day
 
+def GetMAX(ticker):
+	'''
+		Open,High,Low,Close,Volume,Dividends,Stock Splits
+	'''
+	hist = []
+	objtk = yf.Ticker(ticker)
+	data = objtk.history(period="max")
+	for idx, row in data.iterrows():
+		hist.append({
+			"date": "{0}".format(idx),
+			"open": row['Open'],
+			"close": row['Close'],
+			"high": row['High'],
+			"low": row['Low'],
+			"vol": row['Volume']
+		})
+	return hist
+
 def Run(start, stop, sell_interval):
 	global g_exit
+	# Calculate running months
 	months = (stop["year"] - start["year"]) * 12 + (stop["month"] - start["month"])
-
+	# Open local database
 	path = os.path.join("..","stocks.db")
 	conn = sqlite3.connect(path)
 	curs = conn.cursor()
@@ -106,19 +205,21 @@ def Run(start, stop, sell_interval):
 			month = 1
 			year += 1
 		
-		investment 	= 0
-		earnings 	= 0
+		investment 		= 0
 		investment_date = ""
-		earning_date = ""
+		earnings 		= 0
+		earning_date 	= ""
+
 		anomaly_tickers = ""
-		below_dollar = []
-		above_dollar = []
+		below_dollar 	= []
+		above_dollar 	= []
 
 		print("({0}\{1}) {2:.4g}%".format(idx,months,float(idx/months)*100.0))
-
-		y, m, d = GetWorkingDate(year,month,day)
-		stocks, tickers = GetStocks(curs,1,y, m, d)
-		investment_date = "{0}_{1}_{2}".format(y,m,d)
+		print("({0}\{1}) Get (BUY) stocks ...".format(idx,months))
+		stocks, tickers = GetClosestStocks(curs,1,year,month,day)
+		print("({0}\{1}) Total ammount of stocks ({2})".format(idx,months,len(stocks)))
+		investment_date = "{0}_{1}_{2}".format(year,month,day)
+		earning_date 	= "{0}_{1}_{2}".format(year+1,month,day)
 
 		#if prev_tickers is not None:
 		#	for item in prev_tickers:
@@ -129,34 +230,36 @@ def Run(start, stop, sell_interval):
 		#			below_dollar.append(item)
 		#prev_tickers = tickers.copy()
 
-		y, m, d = GetWorkingDate(year+1,month,day)
-		earning_date = "{0}_{1}_{2}".format(y,m,d)
+		#y, m, d = GetWorkingDate(year+1,month,day)
+		#earning_date = "{0}_{1}_{2}".format(y,m,d)
+
 		invested_tickers = []
 		for stock in stocks[:]:
 			if g_exit is True:
 				return
 			
 			if stock["ticker"] not in invested_tickers: # Remove duplication
-				if stock["price"] > 0: # Stock cannot be negative
-					invested_tickers.append(stock["ticker"])
-					investment += stock["price"]
-					# Get stock from future
-					stock_e = GetStock(curs,stock["ticker"],y, m, d)
-					if stock_e is not None: # Stock exist
-						# Append to earnings
-						print("+{0}",format(stock_e["price"]))
-						earnings += stock_e["price"]
-						if stock_e["price"] > float(10 * stock["price"]):
-							print("[WARRNING] Ticker: {0}, Investment: {1} Earning: {2}".format(stock["ticker"], stock["price"], stock_e["price"]))
-							anomaly_tickers += "{0}|".format(stock["ticker"])
-					else: # STock does not exist (migght be bankropsy)
-						print("-{0}",format(stock["price"]))
-						earnings -= stock["price"]
-				else:
-					print("[ERROR] Stock price is NEGATIVE - Ticker: {0}, Price: {1}".format(stock["ticker"], stock["price"]))
+				#if stock["price"] > 0: # Stock cannot be negative
+				invested_tickers.append(stock["ticker"])
+				investment += stock["price"]
+				# Get stock from future
+				stock_e = GetClosestStock(curs,stock["ticker"],year+1,month,day)
+				if stock_e is not None: # Stock exist
+					# Append to earnings
+					#print("+{0}",format(stock_e["price"]))
+					earnings += stock_e["price"]
+					if stock_e["price"] > float(10 * stock["price"]):
+						print("[WARRNING] Ticker: {0}, Investment: {1} Earning: {2}".format(stock["ticker"], stock["price"], stock_e["price"]))
+						anomaly_tickers += "{0}|".format(stock["ticker"])
+				else: # STock does not exist (migght be bankropsy)
+					print("-{0}",format(stock["price"]))
+					earnings -= stock["price"]
+				#else:
+				#	print("[ERROR] Stock price is NEGATIVE - Ticker: {0}, Price: {1}".format(stock["ticker"], stock["price"]))
 			
 				investment += stock["price"]
-		csv += "{0},{1},{2},{3},{4},{5}\n".format(idx, investment, earnings, investment_date, earning_date, anomaly_tickers)
+		print("({0}\{1}) Invested ({2}) Earned ({3})".format(idx,months,investment,earnings))
+		csv += "{0},{1},{2},{3},{4},{5}\n".format(month, investment, earnings, investment_date, earning_date, anomaly_tickers)
 		
 		#row_1.append(investment)
 		#row_2.append(earnings)
@@ -177,46 +280,23 @@ def main():
 	parser.add_argument('--query', action='store_true', help='Create queries from stocks resources')
 	parser.add_argument('--create', action='store_true', help='Crerate DB from queries')
 	parser.add_argument('--tickers', action='store', dest='tickers', help='Append tickers from tickers file')
+	parser.add_argument('--info', action='store', dest='name', help='Read ticker history')
 	args = parser.parse_args()
 
-	Run({
-		"year": 2011,
-		"month": 1,
-		"day": 1
-	},{
-		"year": 2011,
-		"month": 3,
-		"day": 1
-	}, 1)
-
-	'''
-	path = os.path.join("..","stocks.db")
-	conn = sqlite3.connect(path)
-	curs = conn.cursor()
-
-	year, month, day = GetWorkingDate(2008,5,13)
-	stocks = GetStocks(curs,1,year, month, day)
-		
-	investment = 0
-	earnings = 0
-	year, month, day = GetWorkingDate(2008,9,13)
-	for stock in stocks:
-		investment += stock["price"]
-		stock_e = GetStock(curs,stock["ticker"],year, month, day)
-		if stock_e is not None:
-				earnings += stock_e["price"]
-		else:
-			earnings -= stock["price"]
-		print(investment,earnings)
-
-	#query = "SELECT * FROM stocks_info WHERE `ticker` = '{0}'".format(ticker)
-	#curs.execute(query)
-	#rows = curs.fetchall()
-	#if len(rows) > 0:
-	#	pass
-
-	conn.close()
-	'''
+	if args.name is not None:
+		data = GetMAX(args.name)
+		for stock in data:
+			print("{0}\t{1:.2f} \t {2:.2f}".format(stock["date"],stock["open"],stock["close"]))
+	else:
+		Run({
+			"year": 2005,
+			"month": 1,
+			"day": 1
+		},{
+			"year": 2019,
+			"month": 12,
+			"day": 1
+		}, 1)
 
 if __name__ == "__main__":
 	main()
